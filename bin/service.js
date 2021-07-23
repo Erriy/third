e30cff76f9028adb30f9d83299f225e1f7eab717
erriy <Erriy@msn.com>
2021-07-23 16:06:41 +0800

    feat(temp): temp

--- bin/service.js
+++ bin/service.js
@@ -5,10 +5,10 @@ const cmd = new Command('service');
 cmd
     .option('-b, --bootstrap <string>', '指定启动时连接的服务', '')
     .option('-p, --port <number>', '指定服务端口', 34105)
+    .option('-r, --relay <string>', '指定中继服务器', null)
     .action(async (opts) => {
         const bootstrap = opts.bootstrap.split(',').filter(x=>x.length);
-        await service.start({bootstrap, port: opts.port});
-        console.log('service start');
+        await service.start({bootstrap, port: opts.port, relay: opts.relay});
     });
 
 module.exports = cmd;

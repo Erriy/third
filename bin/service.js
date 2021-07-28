31456cc90ab82a3d55e745a9b2c67f32873fab3b
erriy <Erriy@msn.com>
2021-07-28 15:45:19 +0800

    feat: 测试版本，稍后删除

--- bin/service.js
+++ bin/service.js
@@ -3,9 +3,9 @@ const {service} = require('../lib/');
 const cmd = new Command('service');
 
 cmd
-    .option('-b, --bootstrap <string>', '指定启动时连接的服务', '')
+    .option('-b, --bootstrap <string>', '指定启动时连接的服务', 'http://third.on1y.net:5353')
     .option('-p, --port <number>', '指定服务端口', 34105)
-    .option('-r, --relay <string>', '指定中继服务器', null)
+    .option('-r, --relay <string>', '指定中继服务器', 'http://third.on1y.net:5353/')
     .action(async (opts) => {
         const bootstrap = opts.bootstrap.split(',').filter(x=>x.length);
         await service.start({bootstrap, port: opts.port, relay: opts.relay});

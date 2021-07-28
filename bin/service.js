bdef999a02a0ff38e96d6a3e20ee219467a6fcda
erriy <Erriy@msn.com>
2021-07-28 16:25:32 +0800

    feat: test
    test

--- bin/service.js
+++ bin/service.js
@@ -6,9 +6,10 @@ cmd
     .option('-b, --bootstrap <string>', '指定启动时连接的服务', 'http://third.on1y.net:5353')
     .option('-p, --port <number>', '指定服务端口', 34105)
     .option('-r, --relay <string>', '指定中继服务器', 'http://third.on1y.net:5353/')
+    .option('--disable-clipboard', '禁止同步剪贴板', false)
     .action(async (opts) => {
         const bootstrap = opts.bootstrap.split(',').filter(x=>x.length);
-        await service.start({bootstrap, port: opts.port, relay: opts.relay});
+        await service.start({bootstrap, port: opts.port, relay: opts.relay, enable_clipboard: !opts.disableClipboard});
     });
 
 module.exports = cmd;

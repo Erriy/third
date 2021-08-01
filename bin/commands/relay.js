1a2d7ed51fc462e86c9d58d6b8ab748640294a4e
erriy <Erriy@msn.com>
2021-08-01 18:21:58 +0800

    feat(temp): temp

--- bin/commands/relay.js
+++ bin/commands/relay.js
@@ -4,10 +4,18 @@ const {driver} = require('../../lib');
 
 cmd
     .command('create')
-    .requiredOption('-p, --port <number>', '指定要映射的端口')
+    .argument('<port>', '指定要映射的端口')
     .option('-r, --relay <string>', '要使用的relay服务，不指定则与本地服务使用同一relay服务器', null)
-    .action(async (opts)=> {
-        const r = await driver.relay.create({port: opts.port, relay: opts.relay});
+    .action(async (port, opts)=> {
+        const r = await driver.relay.create({port, relay: opts.relay});
+        console.log(r);
+    });
+
+cmd
+    .command('remove')
+    .argument('<port>', '指定要终止映射的端口')
+    .action(async (port)=>{
+        const r = await driver.relay.remove(port);
         console.log(r);
     });
 
@@ -16,6 +24,20 @@ cmd
     .action(async ()=>{
         const r = await driver.relay.list();
         console.log(r);
+    });
+
+cmd
+    .command('get')
+    .argument('<port>', '指定要映射的端口')
+    .action(async (port)=>{
+        const r = await driver.relay.get(port);
+        if(200 === r.code) {
+            console.log(r.relay);
+        }
+        else {
+            console.error(r.message);
+            process.exit(-1);
+        }
     });
 
 module.exports = cmd;

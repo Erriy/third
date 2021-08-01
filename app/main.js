87bcfd18d04e655c5abc6d6add58e0f5b4f4cf9f
erriy <Erriy@msn.com>
2021-08-01 23:26:33 +0800

    feat(temp): temp

--- app/main.js
+++ app/main.js
@@ -2,12 +2,19 @@ const {app, BrowserWindow} = require('electron');
 
 app.on('ready', function () {
     const win = new BrowserWindow({
-        width    : 300,
-        height   : 18,
-        frame    : false,
-        resizable: false,
+        width                 : 300,
+        height                : 500,
+        frame                 : false,
+        resizable             : false,
+        fullscreenable        : false,
+        transparent           : true,
+        visibleOnAllWorkspaces: true,
+        hasShadow             : false
     });
     app.dock.hide();
+    win.setIgnoreMouseEvents(true);
+    win.setAlwaysOnTop(true, 'floating');
+
     // Or load a local HTML file
     win.loadURL(`file://${__dirname}/view/index.html`);
 });

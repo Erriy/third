3b01ca243967627e37781cf1771197e37ab804bb
erriy <Erriy@msn.com>
2021-08-08 16:26:35 +0800

    temp

--- app/main.js
+++ app/main.js
@@ -1,24 +1,35 @@
-const {app, BrowserWindow, Menu, Tray} = require('electron');
+const {app, dialog} = require('electron');
 const path = require('path');
 const service = require('../lib');
 const clipboard = require('./clipboard');
 const api = require('./api');
 const tray = require('./tray');
 
-app.on('ready', async ()=>{
-    try {
-        await service.start({
-            root     : path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], '.third'),
-            port     : 34105,
-            bootstrap: [],
-            service  : ['api'],
+app.on('ready', ()=>{
+    service.start({
+        root: path.join(
+            process.env[
+                (process.platform == 'win32') ?
+                    'USERPROFILE' :
+                    'HOME'
+            ],
+            '.third'),
+        port     : 34105,
+        bootstrap: [],
+        service  : ['api'],
+    }).then(async ()=>{
+        await tray.init();
+        api.init();
+        clipboard.init();
+    }).catch(e=>{
+        dialog.showMessageBox(null, {
+            type   : 'error',
+            message: e
         });
-    }
-    catch(e) {
-        // todo 提醒端口占用
         app.quit();
-    }
-    tray.init();
-    api.init();
-    clipboard.init();
+    });
+});
+
+app.on('window-all-closed', (e)=>{
+    e.preventDefault();
 });
--- app/tray.js
+++ app/tray.js
@@ -1,5 +1,6 @@
-const {app, BrowserWindow, Menu, Tray} = require('electron');
+const {app, BrowserWindow, Menu, Tray, clipboard} = require('electron');
 const path = require('path');
+const {routine} = require('../lib');
 
 const obj = {
     tray: null,
@@ -27,15 +28,65 @@ function create_login_window () {
     }
 }
 
-function init () {
-    obj.tray = new Tray(path.join(__dirname, '../src/assets/logo_16x16.jpg'));
+async function refresh () {
+    const can_change = await routine.account.authority();
+    const thisfpr = routine.runtime.key.getFingerprint().toUpperCase();
 
     obj.tray.setContextMenu(Menu.buildFromTemplate([
         {
-            label: '登录',
-            click: ()=>{
+            label  : '登录',
+            visible: !routine.account.fingerprint,
+            click  : ()=>{
                 create_login_window();
             }
+        },
+        {
+            label  : (routine.account.fingerprint || '').slice(24) + (routine.account.object ? '(已登录)' : '(请求中)'),
+            visible: routine.account.fingerprint,
+            submenu: [
+                {
+                    label: '复制',
+                    click () {
+                        clipboard.writeText(routine.account.fingerprint);
+                    }
+                },
+                {
+                    label: '退出',
+                    click (){
+                        // todo 退出登录
+                    }
+                },
+            ]
+        },
+        {
+            label  : '所有设备',
+            visible: routine.account.device.list().length > 0,
+            submenu: routine.account.device.list().map(f=>{
+                return {
+                    label  : f.slice(24) + (f === thisfpr ? '(本机)' : ''),
+                    submenu: [
+                        {
+                            label  : '删除',
+                            enabled: can_change,
+                            async click () {
+                                await routine.account.device.remove(f);
+                            }
+                        },
+                        {
+                            label: '复制',
+                            click () {
+                                clipboard.writeText(f);
+                            }
+                        },
+                        // todo 给设备发送消息
+                        // {
+                        //     label: '发送',
+                        //     click () {
+                        //     }
+                        // },
+                    ]
+                };
+            })
         },
         {
             label: '退出',
@@ -44,6 +95,14 @@ function init () {
             }
         }
     ]));
+}
+
+async function init () {
+    obj.tray = new Tray(path.join(__dirname, '../src/assets/logo_16x16.jpg'));
+    await refresh();
+    routine.account.on('change', async ()=>{
+        await refresh();
+    });
 }
 
 module.exports = {

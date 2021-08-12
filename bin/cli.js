01440c208867024bb2216db1fc72797d4c17576f
erriy <Erriy@msn.com>
2021-08-12 16:06:05 +0800

    temp

--- bin/cli.js
+++ bin/cli.js
@@ -2,30 +2,22 @@
 const { Command } = require('commander');
 const program = new Command('third');
 const path = require('path');
-const service = require('../lib');
+const engine = require('../lib');
 
 program
-    .option('--root <string>', '指定应用运行时根目录', path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], '.third'))
+    .option('--database <string>', '指定应用运行时数据库文件位置', path.join(process.env.HOME || process.env.USERPROFILE, '.third/database'))
     .option('-b, --bootstrap <string...>', '指定启动时连接的服务', 'http://third.on1y.net:5353')
     .option('-r, --relay <string>', '指定中继服务器', null)
     .option('-p, --port <number>', '指定服务端口号', 34105)
     .option('--enable-relay', '启动relay服务', false)
-    .option('--provider', '支持ticket存储查询服务', false)
+    .option('--disable-mdns', '禁用mdns内网自发现服务', false)
+    .option('--provider', '支持kns存储查询服务', false)
     .action(async (opts) => {
-        const enable_services = ['api'];
-        if(opts.enableRelay) {
-            enable_services.push('relay');
-        }
-        if(opts.relay && !opts.relay.endsWith('/')) {
-            opts.relay += '/';
-        }
-        await service.start({
-            root     : opts.root,
-            port     : opts.port,
-            bootstrap: opts.bootstrap || [],
-            relay    : opts.relay,
-            service  : enable_services,
-            provider : opts.provider,
+        await engine.init({
+            database: opts.database,
+            port    : opts.port,
+            mdns    : !opts.disableMdns,
+            provider: opts.provider,
         });
     });
 
